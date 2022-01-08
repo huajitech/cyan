d933253337a9afd3f84e0dd2dc2fed9a6382a1b0
Ricky8955555 <397050061@qq.com>
2022-01-08 16:00:42 +0800

    refactor: 🎨引入部分类型以便于用户操作

--- cyan/model/__init__.py
+++ cyan/model/__init__.py
@@ -1,3 +1,7 @@
 from cyan.model.user import User as User
 from cyan.model.guild import Guild as Guild
 from cyan.model.member import Member as Member
+from cyan.model.role import Role as Role
+from cyan.model.channel import Channel as Channel
+from cyan.model.channel import ChannelGroup as ChannelGroup
+from cyan.model.channel import ChannelVisibility as ChannelVisibility
