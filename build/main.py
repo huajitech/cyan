67120887168fc6a6b4a5300dae19e8c777272b93
Ricky8955555 <397050061@qq.com>
2022-01-24 21:46:02 +0800

    ci: 💻 配置 Nightly 构建

---
+++ build/main.py
@@ -0,0 +1,27 @@
+import sys
+import toml
+import os
+from datetime import datetime
+
+
+if len(sys.argv) > 2:
+    raise ValueError("More than 1 arguments were got.")
+dir = os.path.abspath(sys.argv[1] if len(sys.argv) == 2 else os.path.curdir)
+print(f"Project path was defined as: {dir}")
+path = os.path.join(dir, "pyproject.toml")
+if not os.path.exists(path):
+    raise FileNotFoundError(f"Could not found project config file: {path}")
+print(f"Project config was found: {path}.")
+with open(path, "r") as fp:
+    print("Loading project config...")
+    config = toml.load(fp)
+print(f"Project config was loaded:\n {config}")
+date = datetime.today().strftime("%Y%m%d")
+version = config["tool"]["poetry"]["version"] + f"-nightly.{date}"
+config["tool"]["poetry"]["version"] = version
+print(f"Version was modified as: {version}")
+with open(path, "w") as fp:
+    print(f"Serializing and writing config to file...")
+    toml.dump(config, fp)
+print("Config was successfully serialized and written to file.")
+print("Config was successfully updated.")
